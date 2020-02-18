a2eb107f4778c65153299f9f5ed476f86fecc839
arifianto <fian@rebelworks.co>
2020-02-18 15:29:00 +0700

    initial commit, splash screen

---
+++ app/src/main/java/com/rebelworks/course/SplashScreen.java
@@ -0,0 +1,14 @@
+package com.rebelworks.course;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+}
